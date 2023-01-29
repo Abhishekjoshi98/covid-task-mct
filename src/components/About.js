c6c083ad822cf01569c227df2ec1a6a6e2b1486f
Abhishekjoshi98 <98544171+Abhishekjoshi98@users.noreply.github.com>
2023-01-29 16:51:37 +0530

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,17 @@
+import { Typography } from '@mui/material'
+import React from 'react'
+
+const About = () => {
+  return (
+    <div style={{backgroundColor:"#243B54", color:"white", textAlign:"center", height:"70vh", padding:"4rem" }}>
+     <Typography variant='h4'>
+       Abhishek Joshi for geekster
+     </Typography>
+     <Typography variant='h6' marginTop={"4rem"}>
+       I'm a Developer. Master's degree in MCA from Cambridge Institute of tech,KR puram,Bangalore. I'm looking forward to work with the organizations which provide an intution of collective belonging, responsibility and an environment that nurtures the ideas of creativity. I have acquired the skills neccessary to build websites.
+     </Typography>
+    </div>
+  )
+}
+
+export default About
